75b1b04bc0c28c09c8fac64344c010a677da845c
pentel13 <bmorelli25@gmail.com>
2016-08-03 16:03:10 -0700

    starting to implement controls.jsx onclick function

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -4,20 +4,23 @@ var Controls = React.createClass({
   propTypes: {
     countdownStatus: React.PropTypes.string.isRequired
   },
+  onStatusChange: function (newStatus) {
+
+  },
   render: function () {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') { //render pause button
-        return <button className="button secondary">Pause</button>;
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>;
       } else if (countdownStatus === 'paused') { //render start button
-        return <button className="button primary">Start</button>;
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>;
       }
     };
 
     return (
       <div className="controls">
         {renderStartStopButton()}
-        <button className="button alert hollow">Reset</button>
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Reset</button>
       </div>
     );
   }
