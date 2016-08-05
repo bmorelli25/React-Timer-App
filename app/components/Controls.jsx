55fe7f05deea73e3f95d24077bc9d431f999e844
pentel13 <bmorelli25@gmail.com>
2016-08-05 08:48:34 -0700

    Countdown is working
    finally.

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,10 +2,13 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
   },
   onStatusChange: function (newStatus) {
-
+    return () =>{
+      this.props.onStatusChange(newStatus);
+    }
   },
   render: function () {
     var {countdownStatus} = this.props;
